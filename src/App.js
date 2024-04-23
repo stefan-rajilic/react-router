e5685e367a831683ae4d9827362f557b3874e28f
stefan-rajilic <stefan.rajilic@seznam.cz>
2024-04-23 10:52:42 +0200

    zaklad na ukol

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Eshop from "./pages/Eshop";
+import ContanctUs from "./pages/ContanctUs";
 
 function App() {
   return (
     <div className="App">
+
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}>
             <Route path="home" element={<Home />} />
+            <Route path="eshop" element={<Eshop />} />
+            <Route path="contactus" element={<ContanctUs />} />
             {/* <Route path="*" element={<NoPage />} /> */}
           </Route>
         </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/home">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" to="/eshop">
+                  Eshop
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" to="/contactus">
+                  Contanct Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
